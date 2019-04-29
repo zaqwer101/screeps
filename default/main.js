b60f45d048e85ac7ade2f4a4a6f11906146aa707
zaqwer101 <platonov256@gmail.com>
2019-04-29 16:37:10 +0800

    Test 2

--- default/main.js
+++ default/main.js
@@ -8,7 +8,7 @@ var upgraderBody = [MOVE, WORK, CARRY];
 
 module.exports.loop = function ()
 {
-    console.log("Github test");
+    console.log("Github test 2");
     var harvestersCount = 0, buildersCount = 0, upgradersCount = 0;
     for(var name in Game.creeps)
     {
